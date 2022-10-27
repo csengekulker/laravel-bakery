6e1a2035a6b52bd4ee45bc8a5b5bcd82d3a34014
csengekulker <k71608883429@ktch.hu>
2022-10-27 07:45:03 -0400

    created migrations

--- database/migrations/2022_10_27_112949_create_products_table.php
+++ database/migrations/2022_10_27_112949_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateTestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tests', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->foreignId('type_id');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateTestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('products');
     }
 }

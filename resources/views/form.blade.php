f1ba5eca831680691d5a7d90a0baad79087d7415
csengekulker <k71608883429@ktch.hu>
2022-10-27 08:32:08 -0400

    frontend layouts

---
+++ resources/views/form.blade.php
@@ -0,0 +1,32 @@
+<h2>termék hozzáadása</h2>
+
+<form action="new-product" method="post">
+
+    {{ csrf_field() }}
+
+    <p>
+        <label for="name">Név:</label>
+        <input type="text" name="name">
+    </p>
+    <p>
+        <label for="price">Ár:</label>
+        <input type="text" name="price">
+    </p>
+    <p>
+        <label for="type">Típus:</label>
+        <input type="text" name="type">
+    </p>
+    <p>
+        <button type="submit">Hozzáad</button>
+    </p>
+    
+</form>
+
+<h2>típus hozzáadása</h2>
+
+<form action="new-type">
+    <p>
+        <label for="type">Típus:</label>
+        <input type="text">
+    </p>
+</form>
---
+++ resources/views/table.blade.php
@@ -0,0 +1,20 @@
+<table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Név</th>
+        <th>Email</th>
+        <th>Telefon</th>
+      </tr>
+    </thead>
+    <!-- <tbody>
+      @foreach( $products as $product )
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->type }}</td>
+            </tr>
+      @endforeach
+    </tbody> -->
+  </table>
